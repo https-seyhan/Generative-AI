b63f50eba1442feac4894c44c0d9512aa60be6ed
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2023-08-26 10:30:18 +1000

    Add files via upload

--- Analyse_pdf_document_with_LLM.py
+++ Analyse_pdf_document_with_LLM.py
@@ -57,6 +57,7 @@ class Document:
     def __convertToText(self, fileName):
         list = []
         print("Working Directory :", os.getcwd())
+        print("All files in the  :", os. listdir())
         with open(fileName, 'rb') as fh:
             for page in PDFPage.get_pages(fh, 
                                         caching=True,
