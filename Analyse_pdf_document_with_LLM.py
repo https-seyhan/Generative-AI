5b2071281516b18e0f264c6ce947e80948429bee
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2023-08-26 13:06:14 +1000

    Add files via upload

--- Analyse_pdf_document_with_LLM.py
+++ Analyse_pdf_document_with_LLM.py
@@ -215,7 +215,7 @@ class Document:
         common_oths = oth_freq.most_common(10)
     
     def __plotTopics(self):
-		print("plotTopics Called !!!")
+        print("plotTopics Called !!!")
         mainTopics = {}
         for key in self.Topics:       
             if key in self.weightsDict:            
