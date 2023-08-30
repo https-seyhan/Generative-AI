01b9785280690633b96f75d83a19c9a01537c9a0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2023-08-30 21:06:28 +1000

    Add files via upload

--- Analyse_pdf_document_with_LLM.py
+++ Analyse_pdf_document_with_LLM.py
@@ -89,7 +89,8 @@ class Document:
         print("textAnalysis Called !!!")
         customize_stop_words = ['a.', 'b.', 'c.', 'i.', 'ii', 'iii', 
         'the', 'to', " \x0c", 'of',
-        '“', '-v-', 'A.', 'B.', '(', ')', 'wlr', 'potential', 'generative', 'new', 'percent', 'use', 'generate', 'high', 'base', 'database','include', '©', 'McKinsey', 'based'
+        '“', '-v-', 'A.', 'B.', '(', ')', 'wlr', 'potential', 'generative', 'new', 'percent', 'use', 'generate', 'high', 'base', 'database','include', '©', 'McKinsey', 'based',
+        'work'
         ]
         for w in customize_stop_words:
             self.nlp.vocab[w].is_stop = True
