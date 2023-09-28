8bb3e51f15e91f19b9f0aa70247545cd33563479
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2023-09-28 23:38:48 +1000

    Add files via upload

--- Analyse_pdf_document_with_LLM.py
+++ Analyse_pdf_document_with_LLM.py
@@ -231,7 +231,7 @@ class Document:
         print("LDA Topics ", self.ldaTopics)
         print("NMF Topics ", self.nmfTopics)
         print("SVD Topics ", self.svdTopics)
-        print("Type of Topics ", type(self.Topics)
+        print("Type of Topics ", type(self.Topics))
 
     def __get_topics(self, vector, vocab):
         num_top_words=10
