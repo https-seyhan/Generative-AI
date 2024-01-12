c0ae0acfcff43ce7cf09fc248f12230c2092222a
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-01-12 18:30:08 +1100

    Add files via upload

--- RE/run_linux_commands.py
+++ RE/run_linux_commands.py
@@ -2,5 +2,6 @@ import os
 stream = os.popen('ls -la')
 output = stream.readlines()
 print(output)
+os.system('ooo_cat USE_CASE_*.odt -o ALL_USE_CASES_test.odt')
 #ooo_cat USE_CASE_*.odt -o ALL_USE_CASES.odt
 
