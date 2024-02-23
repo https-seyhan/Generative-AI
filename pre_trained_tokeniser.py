57f8563a416ee9d361c6d294b5f23c4f5790efef
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-02-23 23:49:08 +1100

    Add files via upload

--- pre_trained_tokeniser.py
+++ pre_trained_tokeniser.py
@@ -3,3 +3,6 @@ from transformers import BertTokenizer
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 tokens = tokenizer.tokenize("Ask users about their preferences (e.g., location, budget, property type)")
 print(tokens)
+
+token_ids = tokenizer.convert_tokens_to_ids(tokens)
+print(token_ids)
