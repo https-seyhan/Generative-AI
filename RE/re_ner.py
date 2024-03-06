82d5067bba20bd9342cd21d59db6f71622ea9126
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-03-06 13:15:07 +1100

    Add files via upload

---
+++ RE/re_ner.py
@@ -0,0 +1,10 @@
+import spacy
+import random
+from spacy.util import minibatch, compounding
+from pathlib import Path
+
+def set_model():
+	print('Model Called!')
+
+if __name__ == '__main__':
+	set_model()
