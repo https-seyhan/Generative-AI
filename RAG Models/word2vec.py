d866d6ad3c4ed8b137e45443d04b7b06d9511ad7
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-03-20 00:40:15 +1100

    Add files via upload

--- RAG Models/word2vec.py
+++ RAG Models/word2vec.py
@@ -1,8 +1,11 @@
 from gensim.models import Word2Vec
-sentences = [["this", "is", "an", "example", "sentence"], ["another", "example", "sentence"]]
+sentences = [["About", "75", "percent", "of", "the", "value", "that", "generative", 
+"AI", "use", "cases", "could", "deliver", "falls", "across", "four", "areas:", "Customer", "operations", "marketing", "and", "sales", "software", "engineering", "and", "R&D"], 
+["The", "era", "of", "generative", "AI", "is", "beginning"], ["Excitement" ,"technology", "is", "palpable", "pilots", "are", "compelling"]]
 # train Word2Vec model
 model = Word2Vec(sentences, vector_size=100, window=5, min_count=1, workers=4)
 # access word vectors
-vector = model.wv['example']
+vector = model.wv['AI']
 print(vector)
+print(len(vector))
 
