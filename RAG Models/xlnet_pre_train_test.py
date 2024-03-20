047d9a4c89cf51f514c8a8febe7633f9138793ac
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-03-21 02:29:46 +1100

    Add files via upload

---
+++ RAG Models/xlnet_pre_train_test.py
@@ -0,0 +1,9 @@
+from transformers import XLNetTokenizer, XLNetModel
+
+tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
+model = XLNetModel.from_pretrained('xlnet-base-cased')
+
+inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+outputs = model(**inputs)
+
+last_hidden_states = outputs.last_hidden_state
