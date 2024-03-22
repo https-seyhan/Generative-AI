091c5c8c395c6c2de72c5be223241a7b7f607445
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-03-22 12:31:01 +1100

    Add files via upload

--- RAG Models/RAG_XLNET_Search_RE_dev.py
+++ RAG Models/RAG_XLNET_Search_RE_dev.py
@@ -47,8 +47,8 @@ print(type(generated_response))
 print("Generated Response:", generated_response)
 
 def compare_output_to_input(generated_response, property_descriptions):
-	print(generated_response)
-	print(property_descriptions)
+	print('Generated Response: ', generated_response, end='\n')
+	print('Property Descriptions: ', property_descriptions)
 
 
 
