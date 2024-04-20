3570912cd6076e3ef143dd003e34ea4e6fee4a95
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-04-20 18:28:23 +1000

    Add files via upload

--- RE/xlnetClassification_dev.py
+++ RE/xlnetClassification_dev.py
@@ -91,7 +91,6 @@ epochs = 5
 max_grad_norm = 1.0
 # Cacluate train optimiazaion num
 
-
 num_train_optimization_steps = int( math.ceil(len(tr_inputs) / batch_num) / 1) * epochs
 
 #Set fine tuning method
