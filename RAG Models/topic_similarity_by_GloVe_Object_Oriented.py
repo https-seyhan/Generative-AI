f49192e2dea4f98e21755e6f21ce3afa76c13201
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-05-04 19:32:44 +1000

    Add files via upload

--- RAG Models/topic_similarity_by_GloVe_Object_Oriented.py
+++ RAG Models/topic_similarity_by_GloVe_Object_Oriented.py
@@ -82,7 +82,7 @@ class GenAI_LLM():
 		# Print the top similar word pairs
 		#for word1, word2, similarity in similar_word_pairs[:10]:
 		#	print(f"{word1} - {word2}: {similarity}")
-		print(similar_word_pairs[:5][:5])
+		print(similar_word_pairs[:5][:5][0])
 
 genAI = GenAI_LLM()
 genAI.get_similars(query_tokens, description_tokens)
