a2348cf8660bb64851ff0166162d47ba30b4601c
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-05-05 10:20:53 +1000

    Add files via upload

--- RAG Models/topic_similarity_by_GloVe_Object_Oriented.py
+++ RAG Models/topic_similarity_by_GloVe_Object_Oriented.py
@@ -86,7 +86,7 @@ class GenAI_LLM():
 		#print(similar_word_pairs[:5][:5][0][0])
 		
 		for query_word in similar_word_pairs[:5]:
-			print(query_word[0])
+			print(query_word[0]) # Get query words
 			
 		
 	def generate_summary(self):
