6b1aed9a71195b0d8aea102d96e6ce2370311971
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-05-08 09:53:25 +1000

    Add files via upload

--- RAG Models/topic_similarity_by_GloVe_Object_Oriented.py
+++ RAG Models/topic_similarity_by_GloVe_Object_Oriented.py
@@ -104,7 +104,7 @@ class GenAI_LLM():
 	def __generate_summary(self, description):
 		print('Genetate Summary Called')
 		doc = nlp(property_descriptions)
-		print(property_descriptions)
+		#print(property_descriptions)
 		pattern = [{"LOWER": description}]
 		matcher.add("description", [pattern])
 		descriptions = nlp(property_descriptions)
