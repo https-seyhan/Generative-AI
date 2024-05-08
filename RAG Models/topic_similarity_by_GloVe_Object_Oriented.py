77512968b078d59661f8c7c1ed5650f34fadf451
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-05-08 14:28:10 +1000

    Add files via upload

--- RAG Models/topic_similarity_by_GloVe_Object_Oriented.py
+++ RAG Models/topic_similarity_by_GloVe_Object_Oriented.py
@@ -122,7 +122,8 @@ class GenAI_LLM():
 		reg_exp = "[^.]*?" + str(word) + "[^.]*\."
 		print('Reg Exp ', reg_exp)
 		#re.findall(r"([^.]*? + str(word) + [^.]*\.)",self.property_descriptions)
-		re.findall(r"(reg_exp)",self.property_descriptions)
+		re = re.findall(r"(reg_exp)",self.property_descriptions)
+		print('RE ', re)
 		#re.findall(r"([^.]*? + str(word) + [^.]*\.)",self.property_descriptions) 
 		#re.findall(r"([^.]*?[^.]*\.)",word, self.property_descriptions_list)  
 		#re.findall(r"([^.]*?apple[^.]*\.)",self.property_descriptions) 
