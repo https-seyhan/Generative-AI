146b0ef8c4f9661a071197ac55b13821be46db58
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-05-11 23:57:52 +1000

    Add files via upload

--- RAG Models/topic_similarity_by_GloVe_Object_Oriented.py
+++ RAG Models/topic_similarity_by_GloVe_Object_Oriented.py
@@ -133,7 +133,7 @@ class GenAI_LLM():
 		
 	def __search_in_description(self, search_item):
 		print('Search in Description ')
-		print('location of the item ', self.property_descriptions.index(search_item))
+		print('location of the item ', search_item, self.property_descriptions.index(search_item))
 
 genAI = GenAI_LLM()
 genAI.get_similars(query_tokens, description_tokens)
