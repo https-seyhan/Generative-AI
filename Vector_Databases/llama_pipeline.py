3d157dee52a5aabe0b5397606d3b9e7bc54bb5a2
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-05-15 10:11:32 +1000

    Add files via upload

---
+++ Vector_Databases/llama_pipeline.py
@@ -0,0 +1,7 @@
+from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
+import os
+
+os.environ['HF_TOKEN'] = "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"
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = "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"
+
+
