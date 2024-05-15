4bf8a600850c89b10f04e632ceb952e7cdce33f4
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-05-15 10:58:34 +1000

    Add files via upload

--- Vector_Databases/llama_pipeline.py
+++ Vector_Databases/llama_pipeline.py
@@ -7,3 +7,11 @@ os.environ['HUGGINGFACEHUB_API_TOKEN'] = "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"
 # Sentiment analysis pipeline
 
 analyzer = pipeline("sentiment-analysis")
+
+# Question answering pipeline, specifying the checkpoint identifier
+
+oracle = pipeline(
+
+    "question-answering", model="distilbert/distilbert-base-cased-distilled-squad", tokenizer="google-bert/bert-base-cased"
+
+)
