07f00a5343c3cf8c7abdcac39a34ac7d8371ded5
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2024-11-27 16:35:51 +1100

    Add files via upload

--- RE/playwright_experiments.py
+++ RE/playwright_experiments.py
@@ -13,6 +13,11 @@ def run(playwright):
     page.goto(page_addr)
     title = page.title()
     print("Page Title:", title)
+    main_heading = page.locator('h1').first.inner_text()
+    print("Main Heading:", main_heading)
+    # Extract text from a specific section
+    #section_text = page.locator('section.featured-news').inner_text()
+    #print(section_text)
     # take a full-page screenshot
     page.screenshot(path='/home/saul/Desktop/RE/screenshots/' + page_addr.sep['.'][0] +'.png', full_page=True)
     # always close the browser
