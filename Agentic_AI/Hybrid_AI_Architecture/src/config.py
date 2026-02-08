3b1d4cb2bcdc3220a036021db9b9eaf72d87831d
Generative AI Specialist | NLP Model Specialist (Large Language Models) |Machine Learning | Deep Learning | Software Enginering <sauldata@tutanota.com>
2026-02-08 19:12:03 +1100

    Add files via upload

---
+++ Agentic_AI/Hybrid_AI_Architecture/src/config.py
@@ -0,0 +1,8 @@
+DATA_PATH = "../data/transactions.csv"
+FEATURE_PATH = "../features/training_features.parquet"
+MODEL_PATH = "../models/fincrime_model.pkl"
+
+OLLAMA_URL = "http://localhost:11434/api/generate"
+LLM_MODEL = "llama3"
+
+EMBEDDING_MODEL = "all-mpnet-base-v2"
